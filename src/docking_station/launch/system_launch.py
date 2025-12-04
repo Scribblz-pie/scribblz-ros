e2a8e0dbf0fb9ef1ae881fec26a65177e03ac861
David Barsoum <davidmbarsoum@gmail.com>
2025-12-04 03:52:21 -0500

    Add docking_station, micro_ros_agent, micro_ros_msgs, raspi_counter_pkg

---
+++ src/docking_station/launch/system_launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    docking_node = Node(
+        package='docking_station',
+        executable='raspi_counter', 
+        name='raspi_counter',
+        output='screen'
+    )
+
+    # micro-ROS agent
+    micro_ros_agent_node = Node(
+        package='micro_ros_agent',
+        executable='micro_ros_agent',
+        name='micro_ros_agent',
+        output='screen',
+        arguments=['udp4', '--port', '8888', '-i', 'wlan1']
+    )
+
+    # Foxglove bridge
+    foxglove_node = Node(
+        package='foxglove_bridge',
+        executable='foxglove_bridge',
+        name='foxglove_bridge',
+        output='screen',
+        arguments=['--ros-args', '--log-level', 'info']
+    )
+
+    return LaunchDescription([
+        docking_node,
+        micro_ros_agent_node,
+        foxglove_node
+    ])
+
---
+++ src/docking_station/setup.py
@@ -0,0 +1,32 @@
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'docking_station'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, glob('launch/*.py')),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='david',
+    maintainer_email='david@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'raspi_counter = docking_station.raspi_counter_pub:main',
+        ],
+    },
+)
